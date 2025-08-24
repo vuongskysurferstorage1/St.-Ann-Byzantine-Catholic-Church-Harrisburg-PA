c83ab99c27558491bdac6a0fa87e29c2267367d1
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-23 22:45:10 -0400

    360 Upload

---
+++ data.js
@@ -0,0 +1,257 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-st-ann-byzantine-catholic-church-harrisburg-pa",
+      "name": "St. Ann Byzantine Catholic Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.9780742590472506,
+        "pitch": 0,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.834775263743987,
+          "pitch": 0.3519867446639946,
+          "rotation": 0,
+          "target": "1-st-ann-byzantine-catholic-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-st-ann-byzantine-catholic-church-harrisburg-pa",
+      "name": "St. Ann Byzantine Catholic Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.382135934691341,
+          "pitch": 0.35928864767236135,
+          "rotation": 0,
+          "target": "0-st-ann-byzantine-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": 0.0073368431307141435,
+          "pitch": 0.16179074176287145,
+          "rotation": 0,
+          "target": "2-st-ann-byzantine-catholic-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-st-ann-byzantine-catholic-church-harrisburg-pa",
+      "name": "St. Ann Byzantine Catholic Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1103289403942664,
+          "pitch": 0.2276136506709463,
+          "rotation": 0,
+          "target": "1-st-ann-byzantine-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": -0.028474058788454215,
+          "pitch": -0.5440929457817028,
+          "rotation": 0,
+          "target": "3-st-ann-byzantine-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": -0.028658846882491318,
+          "pitch": 0.13009955140384832,
+          "rotation": 0,
+          "target": "4-st-ann-byzantine-catholic-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-st-ann-byzantine-catholic-church-harrisburg-pa",
+      "name": "St. Ann Byzantine Catholic Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.01396009631356776,
+          "pitch": 0.91338873766734,
+          "rotation": 0,
+          "target": "2-st-ann-byzantine-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": 0.009035307870075115,
+          "pitch": 0.3039879044905689,
+          "rotation": 0,
+          "target": "1-st-ann-byzantine-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": -3.118813324488274,
+          "pitch": 0.9175307713969243,
+          "rotation": 0,
+          "target": "4-st-ann-byzantine-catholic-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-st-ann-byzantine-catholic-church-harrisburg-pa",
+      "name": "St. Ann Byzantine Catholic Church Harrisburg, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1239502568059443,
+          "pitch": -0.9445714636108349,
+          "rotation": 0,
+          "target": "3-st-ann-byzantine-catholic-church-harrisburg-pa"
+        },
+        {
+          "yaw": -3.1387981880174323,
+          "pitch": 0.07857950278668291,
+          "rotation": 0,
+          "target": "2-st-ann-byzantine-catholic-church-harrisburg-pa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
